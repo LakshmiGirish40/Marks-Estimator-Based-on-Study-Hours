3015b8ecbd8b0fa63bc1c4277212daedc9ed3f21
Lakshm Girish <121075621+LakshmiGirish40@users.noreply.github.com>
2024-10-11 13:47:37 -0500

    Update student_mark_predictor_app.py

--- student_mark_predictor_app.py
+++ student_mark_predictor_app.py
@@ -8,7 +8,7 @@ from sklearn.metrics import mean_squared_error
 
 # Load the saved model
 # Load the model
-model = joblib.load("D:\Data_Science&AI\Spyder\Student_Marks_prediction\student_mark_predictor.pkl")
+model = joblib.load("student_mark_predictor.pkl")
 data = pd.read_csv(r'D:\Data_Science&AI\Spyder\Student_Marks_prediction\student_info.csv')
 student_data_predicted = pd.read_csv("D:\Data_Science&AI\Spyder\Student_Marks_prediction\student_data_prediction.csv")
 #student_data = pd.read_(r"D:\Data_Science&AI\Spyder\Student_Marks_prediction\Student_marks.py")
